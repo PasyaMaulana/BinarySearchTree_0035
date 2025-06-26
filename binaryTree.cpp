cfa8c32cedf0f164bc58f10352352533d76c5021
PasyaMaulana <pasyamaulana17@gmail.com>
2025-06-26 11:59:53 +0700

    jika parent adalah null

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -46,5 +46,15 @@ public:
         Node *parent = nullptr;
         Node *currentNode = nullptr;
         search(x, parent, currentNode);
+
+        // Step 5: If parent is NULL (Tree is empty)
+        if (parent == nullptr)
+        {
+            // 5a: Mark the new node as ROOT
+            ROOT = newNode;
+
+            // 5b: Exit
+            return;
+        }
     }
 };
